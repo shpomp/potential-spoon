f9944f04c09a08b7ddb85bdaf806bacb9a64db3e
shpomp <karolina.urnieziute@gmail.com>
2023-03-04 10:49:38 +0100

    pomclock/rewrite

--- src/App.js
+++ src/App.js
@@ -1,190 +1,202 @@
-import React, { useEffect, useState } from "react";
-
+import React, { useRef, useState } from "react";
 import "./App.css";
+import PomodoroControl from "./components/PomodoroControl";
+import { accurateInterval } from "./utils/helpers.js";
 
-export const App = () => {
-  const [pomodoro, setPomodoro] = useState({
-    mode: "work",
-    breakLength: 5,
-    workLength: 25,
-    timerTime: 0.1 * 60,
-    timerIsActive: false,
-  });
+function App() {
+  const [displayTime, setDisplayTime] = useState(60 * 25);
+  const [breakTime, setBreakTime] = useState(60 * 5);
+  const [sessionTime, setSessionTime] = useState(60 * 25);
+  const [timerOn, setTimerOn] = useState(false);
+  const [onBreak, setOnBreak] = useState(false);
+  const myAudio = useRef();
+  const displayTimerRef = useRef();
+  const timeoutRef = useRef();
+  const onBreakRef = useRef(null);
+  const displayVisualTimerRef = useRef();
 
-  useEffect(() => {
-    console.log(pomodoro.timerTime);
-    let timer = setInterval(() => {
-      if (pomodoro.timerIsActive && pomodoro.timerTime > 0)
-        setPomodoro({ ...pomodoro, timerTime: pomodoro.timerTime - 1 });
-    }, 1000);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  });
-
-  // useEffect(() => {
-  //   if (pomodoro.timerTime === 0) {
-  //     setTimeout(interval, 1000);
-  //     switchMode();
-  //   }
-  // });
-
-  const handleTimeChange = (modeToChange, operation) => {
-    console.log(modeToChange, operation);
-    if (modeToChange === "work") {
-      operation === "inc"
-        ? setPomodoro({
-            ...pomodoro,
-            workLength: pomodoro.workLength + 1,
-            timerTime:
-              60 *
-              (pomodoro.mode === modeToChange
-                ? pomodoro.workLength + 1
-                : pomodoro.breakLength + 1),
-          })
-        : setPomodoro({
-            ...pomodoro,
-            workLength: pomodoro.workLength - 1,
-            timerTime:
-              60 *
-              (pomodoro.mode === modeToChange
-                ? pomodoro.workLength - 1
-                : pomodoro.breakLength - 1),
-          });
-    } else {
-      operation === "inc"
-        ? setPomodoro({
-            ...pomodoro,
-            breakLength: pomodoro.breakLength + 1,
-          })
-        : setPomodoro({
-            ...pomodoro,
-            breakLength: pomodoro.breakLength - 1,
-          });
+  //do the countdown
+  const timerControl = () => {
+    if (!timerOn) {
+      setTimerOn(true);
+      beginCountDown();
+    } else if (timeoutRef.current.timeoutID) {
+      timeoutRef.current.cancel();
+      setTimerOn(false);
     }
   };
 
-  const handleReset = () => {
-    const sound = document.getElementById("beep");
-    sound.pause();
-    sound.currentTime = 0;
-
-    setPomodoro({
-      mode: "work",
-      breakLength: 5,
-      workLength: 25,
-      timerTime: 25 * 60,
-      timerIsActive: false,
-    });
+  const beginCountDown = () => {
+    let timeOutInfo = accurateInterval(() => {
+      phaseControl();
+    }, 1000);
+    timeoutRef.current = timeOutInfo;
+    //console.log(timeoutRef.current.timeoutID)
   };
 
-  const switchMode = () => {
-    playSound();
-    setPomodoro({
-      ...pomodoro,
-      mode: pomodoro.mode === "work" ? "break" : "work",
-      timerTime:
-        pomodoro.mode === "work"
-          ? pomodoro.breakLength * 60
-          : pomodoro.workLength * 60,
-    });
+  const displayElapsedPercentage = (isBreak, currentTick) => {
+    let percentage = "0%";
+    if (isBreak) {
+      percentage =
+        Math.abs(((currentTick - breakTime) / breakTime) * 100) + "%";
+    } else {
+      percentage =
+        Math.abs(((currentTick - sessionTime) / sessionTime) * 100) + "%";
+    }
+    displayVisualTimerRef.current = percentage;
+    //console.log(displayVisualTimerRef.current);
   };
 
-  const playSound = () => {
-    const sound = document.getElementById("beep");
-    console.log(sound);
-    sound.play();
+  const phaseControl = () => {
+    let time;
+
+    setDisplayTime((prev) => {
+      displayTimerRef.current = prev;
+      return prev - 1;
+    });
+
+    time = displayTimerRef.current;
+    //console.log(time)
+
+    if (time <= 0) {
+      setOnBreak((prev) => {
+        let previousState = prev;
+        onBreakRef.current = !previousState;
+        return !prev;
+      });
+
+      if (timeoutRef.current) {
+        timeoutRef.current.cancel();
+        //console.log("canceled")
+
+        if (onBreakRef.current === false) {
+          //console.log("sessionTime" + sessionTime);
+          displayTimerRef.current = sessionTime;
+          playBuzzer();
+        } else if (onBreakRef.current === true) {
+          //console.log("breaktime:" + breakTime);
+          displayTimerRef.current = breakTime;
+          playBuzzer();
+        }
+        setDisplayTime(displayTimerRef.current);
+        beginCountDown();
+      }
+    }
+
+    displayElapsedPercentage(onBreakRef.current, time);
+  };
+
+  //Reset Pomodoro
+  const resetPomodoro = () => {
+    setDisplayTime(60 * 25);
+    setBreakTime(60 * 5);
+    setSessionTime(60 * 25);
+    setOnBreak(false);
+    setTimerOn(false);
+    if (timeoutRef.current) {
+      timeoutRef.current.cancel();
+    }
+    myAudio.current.pause();
+    myAudio.current.currentTime = 0;
+    displayVisualTimerRef.current = "0%";
+  };
+
+  //Pomodoro settings function
+  const changeTime = (timeAmmount, type) => {
+    if (type === "break") {
+      if (
+        (breakTime >= 60 * 60 && timeAmmount > 0) ||
+        (breakTime <= 60 && timeAmmount < 0)
+      ) {
+        return;
+      }
+      setBreakTime((prev) => prev + timeAmmount);
+    } else if (type === "session") {
+      if (
+        (sessionTime >= 60 * 60 && timeAmmount > 0) ||
+        (sessionTime <= 60 && timeAmmount < 0)
+      ) {
+        return;
+      }
+      setSessionTime((prev) => prev + timeAmmount);
+      if (!timerOn) {
+        setDisplayTime(sessionTime + timeAmmount);
+      }
+    }
+  };
+
+  //Helper function to display time
+  const formatTime = (time, type = "display") => {
+    if (type === "display") {
+      let minutes = Math.floor(time / 60);
+      let seconds = time % 60;
+
+      return (
+        (minutes < 10 ? "0" + minutes : minutes) +
+        ":" +
+        (seconds < 10 ? "0" + seconds : seconds)
+      );
+    } else if (type === "control") {
+      let minutes = Math.ceil(time / 60);
+      return minutes;
+    }
+  };
+
+  //helper function to play the buzzer
+  const playBuzzer = () => {
+    myAudio.current.currentTime = 0;
+    myAudio.current.play();
   };
 
   return (
-    <div className="App">
-      <div id="clock">
-        <div id="work">
-          <p id="session-label">WORK</p>
-          <div className="time-controls">
-            <button
-              id="session-decrement"
-              disabled={pomodoro.timerIsActive || pomodoro.workLength === 1}
-              onClick={() => handleTimeChange("work", "dec")}
-            >
-              -
+    <React.Fragment>
+      <h1>FreeCodeCamp Pomodoro Clock App</h1>
+      <main>
+        <header>
+          <div className="session">
+            <PomodoroControl
+              type="break"
+              length={formatTime(breakTime, "control")}
+              changeTime={changeTime}
+            />
+            <PomodoroControl
+              type="session"
+              length={formatTime(sessionTime, "control")}
+              changeTime={changeTime}
+            />
+          </div>
+        </header>
+        <section>
+          <div className="timer">
+            <p id="timer-label" className="title">
+              {onBreak ? "Break" : "Session"}
+            </p>
+            <p id="time-left" ref={displayTimerRef}>
+              {formatTime(displayTime)}
+            </p>
+            <span
+              className="fill"
+              style={{ height: displayVisualTimerRef.current }}
+            ></span>
+          </div>
+          <div className="timer_controls">
+            <button id="start_stop" onClick={() => timerControl()}>
+              {!timerOn ? "Start" : "Pause"}
             </button>
-            <p id="session-length">{pomodoro.workLength}</p>
-            <button
-              id="session-increment"
-              disabled={pomodoro.timerIsActive || pomodoro.workLength >= 60}
-              onClick={() => handleTimeChange("work", "inc")}
-            >
-              +
+            <button id="reset" onClick={() => resetPomodoro()}>
+              Reset
             </button>
           </div>
-        </div>
-
-        <div id="break">
-          <p id="break-label">CHILL</p>
-          <div className="time-controls">
-            <button
-              id="break-decrement"
-              disabled={pomodoro.timerIsActive || pomodoro.breakLength === 1}
-              onClick={() => handleTimeChange("break", "dec")}
-            >
-              -
-            </button>
-
-            <p id="break-length">{pomodoro.breakLength}</p>
-
-            <button
-              id="break-increment"
-              disabled={pomodoro.timerIsActive || pomodoro.breakLength >= 60}
-              onClick={() => handleTimeChange("break", "inc")}
-            >
-              +
-            </button>
-          </div>
-        </div>
-
-        <p id="timer-label">Session</p>
-        <div id="time-left">{formattedTime(pomodoro.timerTime)}</div>
-
-        <button id="start_stop">
-          <img
-            alt="start-button"
-            src={pomodoro.timerIsActive ? "/stop.svg" : "/start.svg"}
-            onClick={() =>
-              setPomodoro({
-                ...pomodoro,
-                timerIsActive: !pomodoro.timerIsActive,
-              })
-            }
-          />
-        </button>
-        <button id="reset">
-          <img
-            alt="reset-button"
-            src={"/reset.svg"}
-            onClick={() => handleReset()}
-          />
-        </button>
-
-        <audio
-          id="beep"
-          src={"/Chicken-Clucking-Short-www.fesliyanstudios.com.mp3"}
-        />
-      </div>
-    </div>
+        </section>
+      </main>
+      <audio
+        id="beep"
+        preload="auto"
+        ref={myAudio}
+        src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+      />
+    </React.Fragment>
   );
-};
+}
 
-const formattedTime = (totalSeconds) => {
-  var minutes = Math.floor(totalSeconds / 60);
-  var seconds = totalSeconds - minutes * 60;
-
-  if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
-  if (seconds < 10) {
-    seconds = "0" + seconds;
-  }
-  return minutes + ":" + seconds;
-};
+export default App;
