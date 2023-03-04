0d6e3bde73e522bcb7aa4bec79bddc6c13b5c146
shpomp <karolina.urnieziute@gmail.com>
2023-03-04 10:05:18 +0100

    pomclock/trying-react-render

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+// import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <>
-    <App />
-  </>
-);
+import { render } from "react-dom";
+
+const domNode = document.getElementById("root");
+render(<App />, domNode);
+
+// const root = ReactDOM.render(document.getElementById("root"));
+// root.render(
+//   <>
+//     <App />
+//   </>
+// );
